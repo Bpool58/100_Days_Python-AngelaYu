42d9af8d8282eb894206f61f5ecca6f0a2907d7f
Branson Poole <bpool58@wgu.edu>
2025-05-26 15:02:15 -0700

    Day 13 Task 3: Fix logic to include 1994 as millennial

--- Day 13/Play Computer/task.py
+++ Day 13/Play Computer/task.py
@@ -1,6 +1,20 @@
-year = int(input("What's your year of birth?"))
+year = int(input("What's your year of birth?")) #For year 1994
 
-if year > 1980 and year < 1994:
+if year > 1980 and year < 1994: #Fixed would be "and year <= 1994:"
     print("You are a millennial.")
 elif year > 1994:
     print("You are a Gen Z.")
+
+
+"""
+Nothing prints because nothing equals the input year of 1994
+"""
+
+
+
+if 1981 <= year <= 1994:
+    print("You are a millennial.")
+elif year > 1994:
+    print("You are a Gen Z.")
+else:
+    print("You are likely a boomer or Gen X.")
